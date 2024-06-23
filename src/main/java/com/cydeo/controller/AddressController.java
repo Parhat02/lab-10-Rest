b9cce3d7504aca7446429b773869c96eb1d4459a
Ferhat <parhat002@gmail.com>
2024-06-23 22:12:54 +0300

    lab-10

--- src/main/java/com/cydeo/controller/AddressController.java
+++ src/main/java/com/cydeo/controller/AddressController.java
@@ -1,6 +1,21 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.AddressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/address")
 public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
 
     /*
      Endpoint: /api/v1/address/{addressNo}
@@ -12,7 +27,14 @@ public class AddressController {
      "code":200
      "data":<address data>
     */
-
+    @GetMapping("{addressNo}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("addressNo") String addressNo){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version", "Cydeo.V1")
+                .header("Operation", "Get Address")
+                .body(new ResponseWrapper("Address successfully retrieved!", addressService.findByAddressNo(addressNo)));
+    }
 
 
     /*
@@ -21,5 +43,12 @@ public class AddressController {
       JSON Response Body:
       <updated address data>
      */
-
+    @PutMapping("{addressNo}")
+    public ResponseEntity<ResponseWrapper> updateAddress(@PathVariable("addressNo") String addressNo, @RequestBody AddressDTO addressDTO){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "Cydeo.V1")
+                .header("Operation", "Create Address")
+                .body(new ResponseWrapper("Course successfully Created!", addressService.update(addressNo, addressDTO)));
+    }
 }

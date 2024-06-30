88fe021738bc1bea920cb2e2b7c2f32d37b72d2e
Ferhat <parhat002@gmail.com>
2024-06-30 22:38:44 +0300

    lab-10

---
+++ src/main/java/com/cydeo/client/WeatherClient.java
@@ -0,0 +1,17 @@
+package com.cydeo.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "http://api.weatherstack.com", name = "WEATHER-CLIENT")
+public interface WeatherClient {
+
+    //http://api.weatherstack.com
+    // /current
+    // ?access_key=87e785a9468054d73eaf989af1e4a99a
+    // &query=Paris
+
+    @GetMapping("/Current")
+    Object getCurrentWeather(@RequestParam("access_key") String accessKey, @RequestParam("query") String city);
+}
